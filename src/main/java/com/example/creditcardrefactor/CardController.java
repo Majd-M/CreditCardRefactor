dc3a71aab6dd6adb084adf898fff387da7ae56be
Majd Mazin <majd.m.mazin@gmail.com>
2022-05-16 03:08:30 -0700

    Images Added

--- src/main/java/com/example/creditcardrefactor/CardController.java
+++ src/main/java/com/example/creditcardrefactor/CardController.java
@@ -3,7 +3,7 @@ package com.example.creditcardrefactor;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class CardController {
     @FXML
     private Label welcomeText;
 
